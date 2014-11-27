efb7190459d314e9ae53682d8e31f9a29ffe7dfb
subbyte <subbyte@gmail.com>
2014-11-27 09:37:49 -0500

    update

--- thinkpad/usr/share/lightdm/session-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# put the file under /usr/share/lightdm/
-
-# left handed mouse except trackpoint and touchpad
-xmodmap -e "pointer = 3 2 1"
-xinput set-button-map "TPPS/2 IBM TrackPoint" 3 2 1
-xinput set-button-map "SynPS/2 Synaptics TouchPad" 3 2 1
